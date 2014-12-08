e81e05526d8316cb99b5afde50c48100ca60f2ba
steven <stevencr@datacom.co.nz>
2014-12-09 11:02:59 +1300

    Initial commit

---
+++ WebSite/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Upload()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/Images"), Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    ViewBag.Message = "File uploaded successfully";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message;
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+            return View(); 
+
+
+        }
+
+        public ActionResult ViewRandom()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
